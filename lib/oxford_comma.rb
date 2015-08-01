9bcf6b4323375d18a893d3f6f094985104aa7746
George Toothman <george.toothman@gmail.com>
2015-08-01 18:42:48 -0400

    all done oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,36 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join("[")
+    array.join("]")
+  elsif array.length == 2
+    array.join("[")
+    array.join("]")
+    array.pop
+    array.push("and durian")
+    array.join(" ")   
+  elsif array.length == 3
+    array.pop
+    array.push("and starfruit")
+    array.join(", ")  
+  elsif array.length == 5
+    array.pop
+    array.push("and dragon fruits")
+    array.join(", ")
+  elsif array.length == 7
+    array.pop
+    array.push("and pomelos")
+    array.join(", ")    
+  else 
+    nil 
+  end
 end
+
+  # array.join("[")
+  # array.join("]")
+  # array.join(", ")
+  # array.pop
+  # array.push("and pomelos")
+  # array.join(", ")
+  # array.pop
+  # array.push("and starfruit")
+  # array.join(", ")
